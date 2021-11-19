63ce490ecf14e251afa27bae11e6e957326c102a
Paul M. Christian <paulmchristian@gmail.com>
2021-11-19 00:33:48 -0600

    concept network

--- src/guitar.js
+++ src/guitar.js
@@ -8,7 +8,7 @@ import {HSVtoHex} from './color';
 //   forceLink
 // } from 'd3-force-3d';
 
-import data from './likes.json';
+// import data from './likes.json';
 
 const tunings = {
   standard: [4, 9, 2, 7, 11, 4],
@@ -38,25 +38,73 @@ osc.on('/likes/*', ({address, args: [note, vel]}) => {
   if (this.onOsc) this.onOsc({instrument, note, vel});
 });
 
-// const data = {
-//   nodes: [
-//     {id:'1'},
-//     {id:'2'},
-//     {id:'3'},
-//     {id:'4'},
-//     {id:'5'},
-//   ],
-//   links: [
-//     {source: '1', target: '2', event: 'pending'},
-//     {source: '1', target: '3', event: 'pending'},
-//     {source: '1', target: '4', event: 'declined'},
-//     {source: '2', target: '3', event: 'pending'},
-//     {source: '2', target: '4', event: 'accepted'},
-//     {source: '2', target: '5', event: 'pending'},
-//     {source: '2', target: '2', event: 'accepted'},
-//     {source: '3', target: '5', event: 'pending'},
-//   ]
-// }
+
+const tags = {
+  '12ET': [
+    'Feynman',
+    'musicality-clj',
+    'musicality-sequencer',
+    'musicality.computer',
+    'pc-clock',
+    'pitch-spiral',
+    'sinewave-organ',
+    'Soundgarden',
+    'VRimba',
+  ],
+  '3D print': [],
+  'JI': [],
+  'MIDI sync': [],
+  'MIDI': [],
+  'OSC': [],
+  'XR object': [],
+  'dataviz': [],
+  'electronic': [],
+  'hardware': [],
+  'instrument': [],
+  'media production': [],
+  'tool': [],
+};
+
+const concepts = [
+  'Earth Under Gold',
+  'Feynman',
+  'musicality-clj',
+  'musicality-sequencer',
+  'musicality.computer',
+  'Oronoco',
+  'pc-clock',
+  'Pheremin',
+  'pitch-spiral',
+  'sinewave-organ',
+  'Soundgarden',
+  'VRimba',
+];
+
+const features = [];
+
+const linkTypes = [
+  'describes',
+  'component of',
+  '',
+
+];
+
+
+const data = {
+  nodes: [
+    ...concepts.map(id => ({id, type: 'concept',})),
+    ..._.keys(tags).map(id => ({id, type: 'tag',})),
+  ],
+  links: [
+    ..._(tags)
+      .map((conceptsTagged, tagId) =>
+        conceptsTagged.map(conceptId => ({source: tagId, target: conceptId, type: 'describes'}))
+      ).flatten()
+      .value(),
+    // {source: '2', target: '1', type: 'describes'},
+    // {source: '3', target: '1', type: 'describes'},
+  ]
+}
 
 window.data = data
 
@@ -72,12 +120,12 @@ const Guitar = () => {
     // Deactivate existing forces
     // fg.d3Force('center', null);
     // fg.d3Force('charge', null);
-    fg.d3Force('charge').strength(-500);
-    fg.d3Force('link').strength(({ event }) => event === 'accepted'
-      ? 0.1
-      : event === 'declined'
-        ? 0.01
-        : 0.05);
+    // fg.d3Force('charge').strength(-500);
+    // fg.d3Force('link').strength(({ event }) => event === 'accepted'
+    //   ? 0.1
+    //   : event === 'declined'
+    //     ? 0.01
+    //     : 0.05);
     // fg.d3ReheatSimulation();
 
 
@@ -86,28 +134,28 @@ const Guitar = () => {
   const handleClick = useCallback(node => {
     // Aim at node from outside it
     const distance = 40;
-    const distRatio = 1 + distance/Math.hypot(node.x, node.y, node.z);
+    const distRatio = 1 + distance / Math.hypot(node.x, node.y, node.z);
 
     const millis = 2000;
 
-    _.delay(() => {
-      window.open(`https://colormelove.worldsingles.com/index.cfm?event=user.view&id=${node.id}`, '_blank');
-    }, millis);
+    // _.delay(() => {
+    //   window.open(`https://colormelove.worldsingles.com/index.cfm?event=user.view&id=${node.id}`, '_blank');
+    // }, millis);
 
     fgRef.current.cameraPosition(
-      { x: node.x * distRatio, y: node.y * distRatio, z: node.z * distRatio }, // new position
+      {x: node.x * distRatio, y: node.y * distRatio, z: node.z * distRatio}, // new position
       node, // lookAt ({ x, y, z })
       millis  // ms transition duration
     );
   }, [fgRef]);
 
-  const linkColor = ({event}) => event === 'accepted'
+  const linkColor = ({type}) => type === 'describes'
     ? '#f4f'
-    : event === 'declined'
-      ? '#0ab'
-      : '#666';
+    : '#666';
 
   const showReceiver = false;
+
+  // TODO: string -> hash -> normalized 0 - 1.0 -> hue
 
   return (
     <ForceGraph3D
@@ -116,9 +164,9 @@ const Guitar = () => {
 
       numDimensions={3}
       graphData={data}
-      nodeOpacity={0.2}
-      nodeLabel={({id, sent, received}) => `${id} s:${sent} r:${received}`}
-      nodeColor={({ sent, received }) => HSVtoHex(0.75 * (sent / (received || 0.1)), Math.min(1, (sent + received) / 4000), 1)}
+      nodeOpacity={0.7}
+      nodeLabel={({id, type}) => `${id} type:${type}`}
+      nodeColor={({type}) => HSVtoHex({concept: 0, tag: 0.1, feature: 0.3}[type] || 0.75, 1, 1)}
       nodeResolution={12}
       nodeRelSize={10}
       onNodeClick={handleClick}
@@ -128,12 +176,12 @@ const Guitar = () => {
       linkDirectionalParticleResolution={1}
       linkDirectionalParticleWidth={1.5}
 
-      linkDirectionalArrowLength={5}
-      linkDirectionalArrowRelPos={showReceiver ? 0.99 : 0.02}
+      // linkDirectionalArrowLength={5}
+      // linkDirectionalArrowRelPos={showReceiver ? 0.99 : 0.02}
 
-      linkOpacity={0.1}
-      linkVisibility={({target}) => target !== '0'}
-      linkColor={linkColor}
+      linkOpacity={0.7}
+      // linkVisibility={({target}) => target !== '0'}
+      // linkColor={linkColor}
       // linkWidth={1}
 
 
@@ -164,7 +212,6 @@ const Guitar = () => {
         document.getElementById('graph')
       );
     });*/
-
 
 
 export default Guitar;
