05cc1ff3dfe0e40e74acfb0141e01f074ac478a2
Paul M. Christian <paulmchristian@gmail.com>
2024-03-25 00:09:28 -0500

    Updates

---
+++ precache-manifest.5be712f685751d6ff05cc545da315608.js
@@ -0,0 +1,18 @@
+self.__precacheManifest = [
+  {
+    "revision": "0ad5ea65bd74767e1556",
+    "url": "/aframe-musicality/static/js/main.4ec903dc.chunk.js"
+  },
+  {
+    "revision": "94355ca965158cfac0f3",
+    "url": "/aframe-musicality/static/js/runtime~main.07aab17b.js"
+  },
+  {
+    "revision": "70702e742cd69f376429",
+    "url": "/aframe-musicality/static/js/2.db3388f1.chunk.js"
+  },
+  {
+    "revision": "3263a6328b7665a6aebac91ac4ab39c1",
+    "url": "/aframe-musicality/index.html"
+  }
+];
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/3.6.3/workbox-sw.js");
 
 importScripts(
-  "/aframe-musicality/precache-manifest.2de7a1585da7500e0ec7a1f47142bfdb.js"
+  "/aframe-musicality/precache-manifest.5be712f685751d6ff05cc545da315608.js"
 );
 
 workbox.clientsClaim();
